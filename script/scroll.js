625f97250334bafe897b0b1286fee61bfd91a448
Charles Pura <164540352+charlespura@users.noreply.github.com>
2025-12-23 00:38:09 +0800

    add scroll effect

---
+++ script/scroll.js
@@ -0,0 +1,18 @@
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        const offset = 64;
+        const bodyRect = document.body.getBoundingClientRect().top;
+        const elementRect = target.getBoundingClientRect().top;
+        const elementPosition = elementRect - bodyRect;
+        const offsetPosition = elementPosition - offset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
